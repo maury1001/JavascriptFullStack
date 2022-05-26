18315288b088bc86cc2fdb4812df416db43d473e
Mauricio <mauricioallaman71@gmail.com>
2022-05-26 11:36:47 -0300

    comit jueves

---
+++ Javascript/index.js
@@ -0,0 +1,38 @@
+let numero = 5 === '5'
+let numero2 = 5 == '5'
+
+console.log(numero)
+console.log(numero2)
+
+let arreglo = [1,2,3,4,5,6,7]
+
+arreglo.forEach(element => {
+
+    console.log(element)
+    
+});
+
+console.log("---------------------------------")
+
+for (let index = 0; index < arreglo.length; index++) {
+    const element = arreglo[index];
+    console.log(element)
+}
+
+while (condition) {
+    
+}
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
