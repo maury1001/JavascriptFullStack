c20f26a4c731db00d90c3b49271a8478000ebcfc
Mauricio <mauricioallaman71@gmail.com>
2022-05-30 11:42:45 -0300

    comit lunes

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,38 +1,62 @@
-let numero = 5 === '5'
-let numero2 = 5 == '5'
+// let numero = 5 === '5'
+// let numero2 = 5 == '5'
 
-console.log(numero)
-console.log(numero2)
+// console.log(numero)
+// console.log(numero2)
 
-let arreglo = [1,2,3,4,5,6,7]
+// let arreglo = [1,2,3,4,5,6,7]
 
-arreglo.forEach(element => {
+// arreglo.forEach(element => {
 
-    console.log(element)
+//     console.log(element)
     
-});
+// });
 
-console.log("---------------------------------")
+// console.log("---------------------------------")
 
-for (let index = 0; index < arreglo.length; index++) {
-    const element = arreglo[index];
-    console.log(element)
-}
+// for (let index = 0; index < arreglo.length; index++) {
+//     const element = arreglo[index];
+//     console.log(element)
+// }
 
-while (condition) {
+// while (condition) {
     
-}
+// }
 
-switch (key) {
-    case value:
+// switch (key) {
+//     case value:
         
-        break;
+//         break;
 
-    default:
-        break;
-}
+//     default:
+//         break;
+// }
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
     
-}
+// }
+
+//funcion callback
+
+// function sumar(a,b,cb){
+//     const r = a+b
+//     cb(r)
+// }
+
+// function callback(result){
+//     console.log('resultado',result);
+// }
+
+// sumar(2,3,callback)
+
+//fat arrow functions
+
+// const miFatArrowFunction = (a,b) => a + b
+// const otraFAF = (a,b) => {
+//     return a+b
+// }
+
+
+// console.log(otraFAF(2,3))
+
