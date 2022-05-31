6283c89f002982b739b4f5080365f531505ab4f8
Mauricio <mauricioallaman71@gmail.com>
2022-05-31 10:19:06 -0300

    comit martes

---
+++ Javascript/main.js
@@ -0,0 +1,21 @@
+const todos = [];
+
+window.onload = () => {
+    const form = document.getElementById('todo-form');
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        const todo = document.getElementById('todo');
+        const todoText = todo.value;
+        todo.value = '';
+        todos.push(todoText);   
+        const todoList = document.getElementById('todo-list');
+        const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+        todoList.innerHTML = todosTemplate.join('')
+        const elementos = document.querySelectorAll('#todo-list li');
+        elementos.forEach((elemento, i) => {
+            elemento.addEventListener('click', () => {
+                console.log(elemento, i)
+            });
+        })
+    }
+}
